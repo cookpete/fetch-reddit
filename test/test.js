6b751ed5ba7c7392e1a700250df1946290428f3a
Pete Cook <pete@cookpete.com>
2015-08-29 14:46:40 +0100

    Stronger tests
    Now tests properties of all extracted posts

--- test/test.js
+++ test/test.js
@@ -12,58 +12,65 @@ import inferTitle from './data/reply-infer-title'
 import selfPost from './data/what_are_your_favorite_i_hate_you_songs'
 
 describe('extractPosts', () => {
-
   it('subreddit', () => {
     let links = extractPosts(subreddit)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 28)
-    links.forEach(assert.isObject)
+    links.forEach(verifyPost)
   })
 
   it('multi', () => {
     let links = extractPosts(multi)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 28)
-    links.forEach(assert.isObject)
+    links.forEach(verifyPost)
   })
 
   it('user', () => {
     let links = extractPosts(user)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 13)
-    links.forEach(assert.isObject)
+    links.forEach(verifyPost)
   })
 
   it('comments', () => {
     let links = extractPosts(comments)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 153)
-    links.forEach(assert.isObject)
+    links.forEach(verifyPost)
   })
 
   it('morechildren', () => {
     let links = extractPosts(morechildren)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 1)
-    links.forEach(assert.isObject)
+    links.forEach(verifyPost)
   })
 
   it('infers a title from text outside URL', () => {
     let links = extractPosts(inferTitle)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 1)
-    links.forEach(assert.isObject)
-    assert.equal(links[0].title, 'Sabre Dance')
-    assert.equal(links[0].url, 'http://www.youtube.com/watch?v=gqg3l3r_DRI')
+    links.forEach(verifyPost)
+    assert.propertyVal(links[0], 'title', 'Sabre Dance')
+    assert.propertyVal(links[0], 'url', 'http://www.youtube.com/watch?v=gqg3l3r_DRI')
   })
 
   it('extracts links from self posts', () => {
     let links = extractPosts(selfPost)
     assert.typeOf(links, 'array')
     assert.lengthOf(links, 90)
-    links.forEach(assert.isObject)
-    assert.equal(links[0].title, 'Everything About You')
-    assert.equal(links[0].url, 'https://www.youtube.com/watch?v=byEGjLU2egA')
+    links.forEach(verifyPost)
+    assert.propertyVal(links[0], 'title', 'Everything About You')
+    assert.propertyVal(links[0], 'url', 'https://www.youtube.com/watch?v=byEGjLU2egA')
   })
-
 })
+
+function verifyPost (post) {
+  assert.isObject(post)
+  assert.property(post, 'id')
+  assert.property(post, 'title')
+  assert.property(post, 'url')
+  assert.property(post, 'created')
+  assert.property(post, 'author')
+}
